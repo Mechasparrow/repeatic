e8e4fcd2ff6d2249566540a18845fa987f2179fb
Mechasparrow <mikipux7@gmail.com>
2018-08-13 22:42:40 -0500

    added serialization and parsing to task model

--- src/models/Task.js
+++ src/models/Task.js
@@ -9,6 +9,22 @@ class Task {
     this.penalty = penalty;
   }
 
+  serialize() {
+    let { label, type, xp, completeBy, penalty } = this;
+    return {
+      label,
+      type,
+      xp,
+      completeBy,
+      penalty
+    };
+  }
+
+  static parse(raw_task) {
+    let { label, type, xp, completeBy, penalty } = raw_task;
+    return new Task(label, type, xp, completeBy, penalty);
+  }
+
   timeLeft() {
     var current_time = moment();
     return current_time.to(this.completeBy);
